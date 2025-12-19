7c21e18970316dc81e205b704a298b957df68e77
HowardLiYH <yuhaoli0204@gmail.com>
2025-12-19 16:40:48 +0800

    chore: Fix trailing newlines

--- data_pipeline/news/__init__.py
+++ data_pipeline/news/__init__.py
@@ -2,4 +2,3 @@
 from .llm_prompt_search import search_micro_macro
 
 __all__ = ["search_micro_macro"]
-
--- data_pipeline/news/providers/__init__.py
+++ data_pipeline/news/providers/__init__.py
@@ -2,4 +2,3 @@
 from .search_serpapi import search as serpapi_search
 
 __all__ = ["serpapi_search"]
-
